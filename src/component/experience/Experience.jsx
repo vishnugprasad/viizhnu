eae4e55bab2fe5ad745e2c3d48e843100ea5279d
vishnugprasad <vishnuanugraha001@gmail.com>
2023-01-27 15:38:15 +0530

    experience

--- src/component/experience/Experience.jsx
+++ src/component/experience/Experience.jsx
@@ -1,10 +1,101 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import {BsShieldFillCheck} from'react-icons/bs'
 
-function Experience() {
+const Experience = () => {
   return (
-    <section id = "experience">Experience</section>
-  )
-}
+    <section id="experience">
+      <h5> What skills I have</h5>
+      <h2> My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience_frontend">
+          <h3>Frontend DEvelopment</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>JavaScript</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>React</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>Tailwind</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
 
-export default Experience
+          </div>
+
+        </div>
+{/* end of frontend */}
+        <div className="experience_backtend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>NodeJS</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>MongoDB</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>Solidity</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+              <h4>RUST</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsShieldFillCheck className="experience__details-icon"/>
+              <div>
+                <h4>AWS</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -15,9 +15,9 @@ const Nav = ()=> {
    <nav>
     <a href="#" onClick={()=>setActiveNav("#")} className ={activeNav === '#' ? 'active' : ''}><AiOutlineHome/> </a>
     <a href='#about' onClick={()=>setActiveNav("#about")}  className ={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/> </a>
-    <a href='#experience'><FiBook/> </a>
-    <a href='#services'><RiServiceLine/> </a>
-    <a href='#contact'><AiOutlineMessage/> </a>
+    <a href='#experience'onClick={()=>setActiveNav("#experience")}  className ={activeNav === '#experience' ? 'active' : ''}><FiBook/> </a>
+    <a href='#services' onClick={()=>setActiveNav("#services")}  className ={activeNav === '#services' ? 'active' : ''}><RiServiceLine/> </a>
+    <a href='#contact' onClick={()=>setActiveNav("#contact")}  className ={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/> </a>
 
    </nav>
   )
