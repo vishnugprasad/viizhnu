4800313b320b5788c9ba52afb930752518988846
vishnugprasad <vishnuanugraha001@gmail.com>
2023-01-29 19:50:37 +0530

    services

--- src/component/services/Services.jsx
+++ src/component/services/Services.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
-function Services() {
+const Services = () => {
   return (
-    <section id ="services">Services</section>
-  )
-}
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className="container_services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI Design</h3>
+          </div>
 
-export default Services
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            
+          </ul>
+        </article>
+        {/* end of UI */}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
