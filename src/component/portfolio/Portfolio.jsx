55f2fca9df5aefe93c2132cea1024def84906f50
vishnugprasad <vishnuanugraha001@gmail.com>
2023-01-30 10:19:36 +0530

    portfolio

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './portfolio.css'
 
-function Portfolio() {
+const  Portfolio = ()=> {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a porfolio item</h3>
+          <a href='https://github.com' className='btn'>Github</a>
+          <a href='https://github.com' className='btn btn-primary'>Demo</a>
+
+        </article>
+      </div>
+    </section>
   )
 }
 
