d08a12a39fa3da90efd867ccb34ec49f889e237b
vishnugprasad <vishnuanugraha001@gmail.com>
2023-01-30 13:04:08 +0530

    portfolio

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -1,22 +1,62 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
 
-const  Portfolio = ()=> {
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Ethereum Token (ERC-20)',
+    github: 'https://github.com/vishnugprasad/EthereumERC20-GP',
+    demo: 'https://github.com/vishnugprasad/EthereumERC20-GP'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'ROAM AROUND EARTH',
+    github: 'https://github.com/vishnugprasad/traveltime',
+    demo: 'https://roamaroundearth.netlify.app/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'OFFICE SEATBOOKING',
+    github: 'https://github.com/vishnugprasad/Hours-subsidary-',
+    demo: 'https://seatbookinghours.netlify.app/'
+  }
+]
+
+const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent work</h5>
       <h2>Portfolio</h2>
-      <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'></div>
-          <h3>This is a porfolio item</h3>
-          <a href='https://github.com' className='btn'>Github</a>
-          <a href='https://github.com' className='btn btn-primary'>Demo</a>
-
+      <div className="container portfolio__container">
+        {
+          data.map(({id,image,title,github,demo})=>{
+            return(
+              <article key={id} className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={image} alt={title}/>
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+          <a href={github} className="btn" target="_blank">Github</a>
+          <a href={demo} className="btn btn-primary"target="_blank">Demo</a>
+          </div>
         </article>
+            )
+          })
+        }
+
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
