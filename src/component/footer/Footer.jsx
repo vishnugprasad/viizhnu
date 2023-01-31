0b6f5f4925950b4dd6b1082005781137af463ac7
vishnugprasad <vishnuanugraha001@gmail.com>
2023-01-31 08:40:04 +0530

    final

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
-function Footer() {
+
+const Footer = ()=> {
   return (
-    <div>Footer</div>
+   <footer>
+    <a href='#' className='footer__logo'>Vishnu G Prasad</a>
+    <ul className='permalinks'>
+      <li><a href='#'>Home</a> </li>
+      <li><a href='#about'>About</a> </li>
+      <li><a href='#experience'>Experience</a> </li>
+      <li><a href='#services'>Services</a> </li>
+      <li><a href='#portfolio'>Portfolio</a> </li>
+      <li><a href='#testimonials'>Endrosment</a> </li>
+      <li><a href='#contact'>Contact</a> </li>
+ 
+   </ul>
+   <div className='footer__socials'>
+   <a href = "https://www.facebook.com/vishnuanugraha001/"><FaFacebookF></FaFacebookF></a>
+   <a href = "https://www.instagram.com/blockchain.unchained/"><FiInstagram></FiInstagram></a>
+   <a href = "https://twitter.com/vishnugprasad"><IoLogoTwitter></IoLogoTwitter></a>
+
+   </div>
+
+
+   </footer>
   )
 }
 
--- src/component/testimonial/Testimonial.jsx
+++ src/component/testimonial/Testimonial.jsx
@@ -1,10 +1,79 @@
-import React from 'react'
-import './testimonial.css'
+import React from "react";
+import "./testimonial.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
 
-function Testimonial() {
+
+// import Swiper core and required modules
+import {Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'name1',
+    review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed ipsum quos voluptatem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed ipsum quos voluptatem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed ipsum quos voluptatem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed ipsum quos voluptatem?"
+  },
+  {
+    avatar: AVTR2,
+    name: 'name2',
+    review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed ipsum quos voluptatem?"
+  },
+  {
+    avatar: AVTR3,
+    name: 'name3',
+    review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed ipsum quos voluptatem?"
+  },
+  {
+    avatar: AVTR4,
+    name: 'name4',
+    review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed ipsum quos voluptatem?"
+  }
+]
+
+
+const Testimonial = () => {
   return (
-    <section id ="testimonial">Testimonial</section>
-  )
-}
+    <section id="testimonial">
+      <h5>Recommendation from friends and colleagues</h5>
+      <h2>Endorsement</h2>
+      <Swiper className="container testimonials__container" 
+       // install Swiper modules
+       modules={[Pagination]}
+       spaceBetween={40}
+       slidesPerView={1}
+      
+       pagination={{ clickable: true }}
+       
+      
+       >
+       
+    {
+      data.map(({avatar,name,review},index)=>{
+        return(
+          <SwiperSlide key = {index}className="testimonial">
+          <div className="client__avatar">
+            <img src={avatar} alt="Avatar" />
+          </div>
+          <h5 className="client__name">{name}</h5>
+          <small className="client__review">
+            {review}
+          </small>
+        </SwiperSlide>
+        )
+      })
+    }
+      </Swiper>
+    </section>
+  );
+};
 
-export default Testimonial
+export default Testimonial;
